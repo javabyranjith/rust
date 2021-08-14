023e7fe09ac5da1f518e6bdb4a75df5fad63b208
Ranjith Sekar <java2ranjith@gmail.com>
2021-08-14 22:05:43 +0530

    Rust experiments

---
+++ rust-exp/src/main.rs
@@ -0,0 +1,28 @@
+// mod printing;
+// mod arguments;
+// mod variables;
+// mod datatypes;
+// mod strings;
+// mod tuples;
+// mod arrays;
+// mod conditions;
+// mod loops;
+// mod functions;
+// mod structs;
+mod enums;
+
+fn main() {
+    // println!("Hello, Rust world!");
+    // arguments::args();
+    // variables::var();
+    // datatypes::types();
+    // strings::str();
+    // tuples::tup()
+    // arrays::run();
+    // conditions::cond();
+    // loops::run();
+    // functions::run();
+    // structs::run();
+    enums::run();
+
+}
